d11d4123d345a6e5403a3797e1f79001485c9d49
ishu-dahi <97476476+ishu-dahi@users.noreply.github.com>
2023-05-31 09:09:28 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-bucket2023
+    bucket = "terraform-state-bucket2023"
     key = "main"
     region = "us-west-1"
     dynamodb_table = "my-dynamodb-table"
